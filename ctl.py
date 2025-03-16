cd5fbc35113eee813c46787a13921a3b7c3e8658
Thomas M <thomasm6m6@gmail.com>
2025-03-16 16:42:29 -0700

    Refactor a bit, add json parsing, shorten the track and add a partial implemenation of grid management

--- ctl.py
+++ ctl.py
@@ -1,73 +1,45 @@
-import argparse
 import math
 import subprocess
 import sys
 import time
+import json
 from abc import ABC, abstractmethod
 
 # TODO refactor to use a Point or something instead of raw x/y pairs
 
+# source: /Library/Developer/CommandLineTools/SDKs/MacOSX14.5.sdk/System/Library/Frameworks/
+# Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h
+
 class Keys:
-    ZL = 0x0C       # Q
-    L = 0x0E        # E
-    MINUS = 0x1b    # -
+    ZL      = 0x0C    # Q
+    L       = 0x0E    # E
+    MINUS   = 0x1B    # -
 
-    ZR = 0x1F       # O
-    R = 0x20        # U
-    PLUS = 0x18     # =
+    ZR      = 0x1F    # O
+    R       = 0x20    # U
+    PLUS    = 0x18    # =
 
-    A_BTN = 0x06    # Z
-    B_BTN = 0x07    # X
-    X_BTN = 0x08    # C
-    Y_BTN = 0x09    # V
+    A_BTN   = 0x06    # Z
+    B_BTN   = 0x07    # X
+    X_BTN   = 0x08    # C
+    Y_BTN   = 0x09    # V
 
-    L_BTN = 0x03    # F
-    L_LEFT = 0x0D   # W
-    L_RIGHT = 0x00  # A
-    L_DOWN = 0x01   # S
-    L_UP = 0x02     # D
+    L_BTN   = 0x03    # F
+    L_LEFT  = 0x0D    # W
+    L_RIGHT = 0x00    # A
+    L_DOWN  = 0x01    # S
+    L_UP    = 0x02    # D
 
-    R_BTN = 0x04    # H
-    R_LEFT = 0x22   # I
-    R_RIGHT = 0x28  # K
-    R_DOWN = 0x26   # J
-    L_UP = 0x25     # L
+    R_BTN   = 0x04    # H
+    R_LEFT  = 0x22    # I
+    R_RIGHT = 0x28    # K
+    R_DOWN  = 0x26    # J
+    R_UP    = 0x25    # L
 
-    D_LEFT = 0x7B   # left arrow
-    D_RIGHT = 0x7C  # right arrow
-    D_DOWN = 0x7D   # down arrow
-    D_UP = 0x7E     # up arrow
-
-class DvorakKeys:
-    ZL = 0x07       # x
-    L = 0x02        # d
-    MINUS = 0x1B    # quote
-
-    ZR = 0x01       # s
-    R = 0x03        # f
-    PLUS = 0x1E     # ]
-
-    A_BTN = 0x2C    # slash
-    B_BTN = 0x0B    # b
-    X_BTN = 0x22    # i
-    Y_BTN = 0x2F    # period
-
-    L_BTN = 0x10    # y
-    L_LEFT = 0x2B   # comma
-    L_RIGHT = 0x00  # a
-    L_DOWN = 0x29   # semicolon
-    L_UP = 0x04     # h
-
-    R_BTN = 0x27    # j
-    R_LEFT = 0x05   # g
-    R_RIGHT = 0x09  # v
-    R_DOWN = 0x08   # c
-    L_UP = 0x23     # p
-
-    D_LEFT = 0x7B   # left arrow
-    D_RIGHT = 0x7C  # right arrow
-    D_DOWN = 0x7D   # down arrow
-    D_UP = 0x7E     # up arrow
+    D_LEFT  = 0x7B    # Left arrow
+    D_RIGHT = 0x7C    # Right arrow
+    D_DOWN  = 0x7D    # Down arrow
+    D_UP    = 0x7E    # Up arrow
 
 class Object:
     def __init__(self, name, category, list_index, index):
@@ -79,6 +51,7 @@ class Object:
     def __repr__(self):
         return f"Object({self.name}, {self.category}, list={self.list_index}, index={self.index})"
 
+# TODO: store object size, validate json on load to make sure there are no overlapping units
 class ObjectManager:
     def __init__(self, style):
         self.objects_by_name = {}
@@ -245,21 +218,24 @@ class ObjectManager:
         self.current_list = list_index
         self.list_indices[list_index] = item_index
 
-# TODO factor into a module maybe
+class GridPoint:
+    # uses 1-based indexing
+    def __init__(self, x_ind, y_ind):
+        self.x = int(x_ind * 915 / 24 - 915 / 48)
+        self.y = int(y_ind * 495 / 14 - 495 / 28 + 78)
+
+# TODO refactor into a module maybe
 # Before initializing this class: have the emulator started, the game loaded, and have clicked "make game"
 class AutomationManager(ABC):
-    # source: /Library/Developer/CommandLineTools/SDKs/MacOSX14.5.sdk/System/Library/Frameworks/
-    # Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h
-
     def __init__(self, x, y, width, height, style, dvorak=False):
         self.win_x = x
         self.win_y = y
         self.win_width = width
         self.win_height = height
 
-        self.keys = DvorakKeys if dvorak else Keys
         self.object_manager = ObjectManager(style)
         self.current_object = ""
+        self.last_inserted_object = None  # Track last inserted object
 
         self._activate()
         self._resize_and_move(x, y, width, height)
@@ -280,54 +256,86 @@ class AutomationManager(ABC):
     def _keystroke(self, key, duration = 100_000):
         pass
 
+    def reset(self):
+        self._click(875, 530, 3_000_000)
+        time.sleep(3)
+
+    def _click_point(self, point, duration = 100_000):
+        self._click(point.x, point.y, duration)
+
     def erase(self, x, y):
-        self._keystroke(self.keys.L)
+        self._keystroke(Keys.L)
+        self._keystroke(Keys.X_BTN)
         self._click(x, y)
-        self._keystroke(self.keys.L)
+        self._keystroke(Keys.L)
+        self._keystroke(Keys.X_BTN)
+
+    def erase_point(self, point):
+        self.erase(point.x, point.y)
+
+    def obstructed_by_menu(self, grid_point):
+        return grid_point.x < 5 or grid_point.x > 22 or grid_point.y < 4
 
     def select_object(self, object_name):
+        if object_name == self.last_inserted_object:
+            return  # Skip re-selection if same object
+
         try:
             obj = self.object_manager.get_object(object_name)
         except ValueError as e:
             print(f"Error: {e}", file=sys.stderr)
             return
 
-        self._keystroke(self.keys.D_UP)
-        self._keystroke(self.keys.Y_BTN)
+        self._keystroke(Keys.D_UP)
+        self._keystroke(Keys.Y_BTN)
         time.sleep(0.5)
 
         current_list = self.object_manager.current_list
         delta = obj.list_index - current_list
-        key = self.keys.R if delta > 0 else self.keys.L
-        for i in range(abs(delta)):
+        key = Keys.R if delta > 0 else Keys.L
+        for _ in range(abs(delta)):
             self._keystroke(key)
 
         current_item = self.object_manager.list_indices[obj.list_index]
         delta = obj.index - current_item
-        key = self.keys.D_RIGHT if delta > 0 else self.keys.D_LEFT
-        for i in range(abs(delta)):
+        key = Keys.D_RIGHT if delta > 0 else Keys.D_LEFT
+        for _ in range(abs(delta)):
             self._keystroke(key)
 
-        self._keystroke(self.keys.A_BTN)
+        self._keystroke(Keys.A_BTN)
         time.sleep(0.2)
 
         self.object_manager.update_selection(obj.list_index, obj.index)
 
-    def place_object(self, object_name, x, y):
+    def place_object(self, object_name, grid_point):
         self.select_object(object_name)
+        self.last_inserted_object = object_name
+        obstructed = self.obstructed_by_menu(grid_point)
 
-        # TODO flesh out grid management
-        self.erase(x, y)
-        self._click(x, y)
+        if obstructed:
+            self._keystroke(Keys.X_BTN)
+            time.sleep(0.2)
+
+        self._click_point(grid_point)
+        time.sleep(0.2)
+
+        if obstructed:
+            self._keystroke(Keys.X_BTN)
+            time.sleep(0.2)
 
     def shorten_track(self):
-        self._click(770, 550) # open track scrubber
-        time.sleep(0.5)
-        self._click(370, 550) # select end of track
-        time.sleep(0.2)
-        self._click(500, 500) # close scrubber
-        time.sleep(0.2)
-        self._click(550, 520, 3_000_000) # make track shorter
+        self._keystroke(Keys.D_DOWN)
+        time.sleep(0.3)
+        self._keystroke(Keys.D_RIGHT)
+        time.sleep(0.3)
+        self._keystroke(Keys.D_RIGHT)
+        time.sleep(0.3)
+        self._keystroke(Keys.A_BTN)
+        time.sleep(0.3)
+        self._click(500, 500)
+        self._click(550, 520, 4_000_000) # make track shorter
+        self._click(500, 500) # restore focus
+
         # for i in range(20):
         #     self._click(550, 520)
         #     time.sleep(0.1)
@@ -362,20 +370,26 @@ def get_automation_manager(x, y, width, height, style, dvorak=False):
     else:
         raise NotImplementedError(f"Unsupported platform '{sys.platform}'")
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dvorak", action='store_true')
-    return parser.parse_args()
-
 def main():
-    args = parse_args()
-
-    am = get_automation_manager(100, 100, 915, 600, "Super Mario Bros", dvorak=args.dvorak)
-
-    # am.place_object("Koopa Troopa", 550, 250) # random square on grid
-    # time.sleep(2)
-    # am.place_object("Bowser Jr.", 550, 250)
+    am = get_automation_manager(100, 100, 915, 600, "Super Mario Bros")
+    am.reset()
     am.shorten_track()
+    time.sleep(0.1)
+
+    with open("x.json") as f:
+        data = json.load(f)
+
+    for unit in data:
+        am.place_object(unit['name'], GridPoint(unit['x'], unit['y']))
 
 if __name__ == "__main__":
     main()
+
+# minimum track grid size is 14 points in height and 24 points in width
+# moving the cursor to a (pixel) point less than y=350 will shift the view up. BUT, simply clicking works fine without adjusting the view.
+# there is a maximum number of each unit that can be added, differs per unit
+# - more fun: the limit applies to groups, so placing a bunch of one unit limits the number of other units
+# cannot place any units in the southwest corner (7 width, 5 height), or in the southeast corner (10 width, 2 height)
+# it appears to be possible to cover the flagpole with units, but a bad idea
+# want to trigger the reset button before we start adding things, IF the game prepopulates anything. I'm not certain if it does.
+# bug: if the object in the square below protrudes into the square above (e.g. Koopa Troopa), clicking on the protruding part will fail to register as a click in the above square
